b7238cf28e214fecf6413c1ac5967089c72f5566
piyush <piyushyadav832988@gmail.com>
2025-06-17 15:39:28 +0530

    registeration page

---
+++ frontend/FoodFleet/src/pages/Register.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../index.css';
+
+const Register = () => (
+  <div className="RegistrationContainer">
+    <div className="registration">
+      <h2>Register</h2>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" required />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </div>
+);
+
+export default Register;
